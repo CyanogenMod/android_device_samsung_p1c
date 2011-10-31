443862fc17084d3133e8bf47b432af819468f394
jt1134 <jt1134@gmail.com>
2011-10-31 16:22:33 -0500

    add support for dbdata partition and use it for dalvik-cache

--- updater.sh
+++ updater.sh
@@ -11,11 +11,13 @@ export PATH=/:/sbin:/system/xbin:/system/bin:/tmp:$PATH
 /tmp/busybox umount -l /system
 /tmp/busybox umount -l /cache
 /tmp/busybox umount -l /data
+/tmp/busybox umount -l /dbdata
 
 # create directories
 /tmp/busybox mkdir -p /system
 /tmp/busybox mkdir -p /cache
 /tmp/busybox mkdir -p /data
+/tmp/busybox mkdir -p /dbdata
 
 # make sure sdcard is mounted
 if ! /tmp/busybox grep -q /sdcard /proc/mounts ; then
@@ -50,14 +52,21 @@ if ! /tmp/busybox mount -t ext4 /dev/block/mmcblk0p1 /cache ; then
 fi
 
 # format data if not ext4
-if ! /tmp/busybox mount -t ext4 /dev/block/mmcblk0p2  /data ; then
+if ! /tmp/busybox mount -t ext4 /dev/block/mmcblk0p2 /data ; then
     /tmp/busybox umount /data
     /tmp/make_ext4fs -b 4096 -g 32768 -i 8192 -I 256 -a /data /dev/block/mmcblk0p2
+fi
+
+# format dbdata if not ext4
+if ! /tmp/busybox mount -t ext4 /dev/block/stl10 /dbdata ; then
+    /tmp/busybox umount /dbdata
+    /tmp/make_ext4fs -b 4096 -g 32768 -i 8192 -I 256 -a /dbdata /dev/block/stl10
 fi
 
 # unmount everything
 /tmp/busybox umount -l /system
 /tmp/busybox umount -l /cache
 /tmp/busybox umount -l /data
+/tmp/busybox umount -l /dbdata
 
 exit 0
