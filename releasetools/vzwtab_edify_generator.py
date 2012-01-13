bd286b479b1363cf9b2e487540b935be78253fa5
KhasMek <Boushh@gmail.com>
2012-01-13 09:40:15 -0700

    Make installer script slightly more verbose
    Change-Id: If83cbd7c2023f41d25ba250dc7185503a8d20e92

--- releasetools/vzwtab_edify_generator.py
+++ releasetools/vzwtab_edify_generator.py
@@ -39,6 +39,7 @@ class EdifyGenerator(edify_generator.EdifyGenerator):
       self.script.append('package_extract_file("boot.img", "/tmp/boot.img");')
       self.script.append('assert(run_program("/tmp/updater.sh") == 0);')
       self.script.append('ui_print("Formatting of EXT4 complete...");')
+      self.script.append('ui_print("Installing system...");')
 
     def RunBackup(self, command):
       edify_generator.EdifyGenerator.RunBackup(self, command)
