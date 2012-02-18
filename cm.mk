b5d4ea40f70fce4143791c0223444c22b83d65c1
jt1134 <jt1134@gmail.com>
2012-02-18 05:04:41 -0600

    add cm.mk
    Change-Id: I703fda35670902a5d7ee7e96cedf2fe4e76faab2

---
+++ cm.mk
@@ -0,0 +1,18 @@
+# Release name
+PRODUCT_RELEASE_NAME := VZWTab
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/vzwtab/full_vzwtab.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := vzwtab
+PRODUCT_NAME := cm_vzwtab
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SCH-I800
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SCH-I800 TARGET_DEVICE=SCH-I800 BUILD_ID=GINGERBREAD BUILD_FINGERPRINT=verizon/SCH-I800/SCH-I800:2.3.4/GINGERBREAD/EF01:user/release-keys PRIVATE_BUILD_DESC="SCH-I800-user 2.3.4 GINGERBREAD EF01 release-keys"
+
