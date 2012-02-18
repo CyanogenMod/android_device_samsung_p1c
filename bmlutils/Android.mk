d91732e815229411b2f59ece84be42d37c0055d8
jt1134 <jt1134@gmail.com>
2012-02-17 19:28:40 -0600

    install bmlwrite to /system/xbin for later use
    Change-Id: I4bd4c741f62ee4b97ddac497af86dfafcd3d0794

--- bmlutils/Android.mk
+++ bmlutils/Android.mk
@@ -1,11 +1,11 @@
-ifneq ($(BOARD_USES_BML_OVER_MTD),true)
+ifeq ($(TARGET_DEVICE),vzwtab)
 
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := bmlwrite
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 LOCAL_SRC_FILES := bmlwrite.c
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_STATIC_LIBRARIES += libstdc++ libc
