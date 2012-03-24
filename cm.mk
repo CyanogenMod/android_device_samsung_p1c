29d045bc827ec4a9d0742cf3a0c1de6b4a05ac5d
jt1134 <jt1134@gmail.com>
2012-03-23 23:56:48 -0500

    set bootanimation resolution
    not perfect, but fits better than the other resolutions
    
    Change-Id: I9a5a985aa5ae5b81b1882f6cf4317f8f432c8d10

--- cm.mk
+++ cm.mk
@@ -1,6 +1,8 @@
 # Release name
 PRODUCT_RELEASE_NAME := P1C
 
+TARGET_BOOTANIMATION_NAME := vertical-540x960
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
