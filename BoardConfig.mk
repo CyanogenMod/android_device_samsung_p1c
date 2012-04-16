b7a71b428eccb36fde0ee2d6bf53f37231979778
jt1134 <jt1134@gmail.com>
2012-04-16 16:35:38 -0500

    fix TARGET_KERNEL defines
    Change-Id: I021ae1be33c3f9d3691295229b353f86e77594e2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,8 +26,8 @@ TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/p1c/releasetools/p
 TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/p1c/releasetools/p1c_img_from_target_files
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/samsung/p1
-TARGET_KERNEL_CONFIG := cyanogenmod_p1_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/p1c
+TARGET_KERNEL_CONFIG := cyanogenmod_p1c_defconfig
 
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := galaxytab7c,p1c,SCH-I800,SPH-P100,vzwtab
