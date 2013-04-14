eb01a9eab3f91eb04ee7910c055d1d17ed188f67
Chirayu Desai <cdesai@cyanogenmod.org>
2013-04-14 10:00:36 +0530

    add usb.rc (moved from p1-common)
    Change-Id: I6c5b13ce0a2d52b2560f148d6303e82bd100d486

--- p1c.mk
+++ p1c.mk
@@ -45,7 +45,9 @@ PRODUCT_COPY_FILES := \
     device/samsung/p1c/fstab.p1c:root/fstab.p1c \
     device/samsung/p1c/init.p1c.rc:root/init.p1c.rc \
     device/samsung/p1c/ueventd.p1c.rc:root/ueventd.p1c.rc \
-    device/samsung/p1-common/init.recovery.rc:root/init.recovery.p1c.rc
+    device/samsung/p1-common/init.recovery.rc:root/init.recovery.p1c.rc \
+    device/samsung/p1c/init.p1c.usb.rc:root/init.p1c.usb.rc \
+    device/samsung/p1c/init.p1c.usb.rc:recovery/root/usb.rc
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
